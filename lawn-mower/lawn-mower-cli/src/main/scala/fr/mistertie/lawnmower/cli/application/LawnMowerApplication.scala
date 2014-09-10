7a5b3fff0c25beb37d3d8ee3d7e2caa928c61664
Mister Tie <postmaster@mister-tie.fr>
2014-09-10 21:06:45 +0200

    fix(cli): Display error messages to standard error before exiting.

--- lawn-mower/lawn-mower-cli/src/main/scala/fr/mistertie/lawnmower/cli/application/LawnMowerApplication.scala
+++ lawn-mower/lawn-mower-cli/src/main/scala/fr/mistertie/lawnmower/cli/application/LawnMowerApplication.scala
@@ -5,8 +5,8 @@ import fr.mistertie.lawnmower.cli.launcher.LawnMowerLauncher.launch
 import fr.mistertie.lawnmower.cli.parser.CommandLineParser
 import fr.mistertie.lawnmower.cli.parser.CommandLineParser.parseArguments
 
+import scala.Console.err
 import scala.sys.exit
-import scala.util.Try
 
 /**
  * Main class of the project that must remain an entry point with as little logic as possible. Delegate the arguments
@@ -15,7 +15,7 @@ import scala.util.Try
 object LawnMowerApplication extends App {
 
   parseArguments(args) match {
-    case Some(arguments) => Try(launch(arguments.file)).recover { case _ => exit(2)}
+    case Some(arguments) => launch(arguments.file).recover { case e: Throwable => err.println(e.getMessage); exit(2)}
     case None => exit(1)
   }
 
